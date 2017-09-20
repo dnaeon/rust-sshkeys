cf3ccd927c8476aa94ecdee8eaa617c6ef5bc362
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-09-20 10:49:18 +0300

    lib: re-export `writer` module

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 extern crate base64;
 extern crate byteorder;
+extern crate sha2;
 
 // TODO: Should all be public?
 pub mod cert;
 pub mod reader;
+pub mod writer;
 pub mod error;
 pub mod pubkey;
 pub mod keytype;
 
 pub use self::cert::{CertType, Certificate};
 pub use self::reader::Reader;
+pub use self::writer::Writer;
 pub use self::error::{Error, ErrorKind, Result};
 pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey};
 pub use self::keytype::{KeyType, KeyTypeKind};
