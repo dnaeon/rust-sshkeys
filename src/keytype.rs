52e23272e701653976127660cb4c2fe033aaf234
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-09-25 12:39:07 +0300

    keytype: add missing ECDSA key types and `plain` field
    The `plain` field returns the cert-less equivalent of a certified
    key type.

--- src/keytype.rs
+++ src/keytype.rs
@@ -2,14 +2,18 @@ use super::error::{Error, ErrorKind, Result};
 
 #[derive(Debug, PartialEq)]
 pub enum KeyTypeKind {
-    KeyRsa,
-    KeyDsa,
-    KeyEcdsa,
-    KeyEd25519,
-    KeyRsaCert,
-    KeyDsaCert,
-    KeyEcdsaCert,
-    KeyEd25519Cert,
+    Rsa,
+    Dsa,
+    EcdsaNistp256,
+    EcdsaNistp384,
+    EcdsaNistp521,
+    Ed25519,
+    RsaCert,
+    DsaCert,
+    EcdsaNistp256Cert,
+    EcdsaNistp384Cert,
+    EcdsaNistp521Cert,
+    Ed25519Cert,
 }
 
 // The `KeyType` represents the type of an OpenSSH key.
@@ -19,109 +23,107 @@ pub struct KeyType {
     pub short_name: &'static str,
     pub is_cert: bool,
     pub kind: KeyTypeKind,
+    pub plain: &'static str, // The cert-less equivalent to a certified key type
 }
 
 impl KeyType {
     pub fn from_name(name: &str) -> Result<KeyType> {
         let kt = match name {
-            "ssh-ed25519" =>
-                KeyType {
-                    name: "ssh-ed25519",
-                    short_name: "ED25519",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyEd25519,
-                },
             "ssh-rsa" =>
                 KeyType {
                     name: "ssh-rsa",
+                    plain: "ssh-rsa",
                     short_name: "RSA",
                     is_cert: false,
-                    kind: KeyTypeKind::KeyRsa,
-                },
-            "rsa-sha2-256" =>
-                KeyType {
-                    name: "rsa-sha2-256",
-                    short_name: "RSA",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyRsa,
-                },
-            "rsa-sha2-512" =>
-                KeyType {
-                    name: "rsa-sha2-512",
-                    short_name: "RSA",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyRsa,
-                },
-            "ssh-dss" =>
-                KeyType {
-                    name: "ssh-dss",
-                    short_name: "DSA",
-                    is_cert: false,
-                    kind: KeyTypeKind:: KeyDsa,
-                },
-            "ecdsa-sha2-nistp256" =>
-                KeyType {
-                    name: "ecdsa-sha2-nistp256",
-                    short_name: "ECDSA",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyEcdsa,
-                },
-            "ecdsa-sha2-nistp384" =>
-                KeyType {
-                    name: "ecdsa-sha2-nistp384",
-                    short_name:
-                    "ECDSA",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyEcdsa,
-                },
-            "ecdsa-sha2-nistp521" =>
-                KeyType {
-                    name: "ecdsa-sha2-nistp521",
-                    short_name: "ECDSA",
-                    is_cert: false,
-                    kind: KeyTypeKind::KeyEcdsa,
-                },
-            "ssh-ed25519-cert-v01@openssh.com" =>
-                KeyType {
-                    name: "ssh-ed25519-cert-v01@openssh.com",
-                    short_name: "ED25519-CERT",
-                    is_cert: true,
-                    kind: KeyTypeKind::KeyEd25519Cert,
+                    kind: KeyTypeKind::Rsa,
                 },
             "ssh-rsa-cert-v01@openssh.com" =>
                 KeyType {
                     name: "ssh-rsa-cert-v01@openssh.com",
+                    plain: "ssh-rsa",
                     short_name: "RSA-CERT",
                     is_cert: true,
-                    kind: KeyTypeKind::KeyRsaCert,
+                    kind: KeyTypeKind::RsaCert,
+                },
+            "ssh-dss" =>
+                KeyType {
+                    name: "ssh-dss",
+                    plain: "ssh-dss",
+                    short_name: "DSA",
+                    is_cert: false,
+                    kind: KeyTypeKind:: Dsa,
                 },
             "ssh-dss-cert-v01@openssh.com" =>
                 KeyType {
                     name: "ssh-dss-cert-v01@openssh.com",
+                    plain: "ssh-dss",
                     short_name: "DSA-CERT",
                     is_cert: true,
-                    kind: KeyTypeKind::KeyDsaCert,
+                    kind: KeyTypeKind::DsaCert,
+                },
+            "ecdsa-sha2-nistp256" =>
+                KeyType {
+                    name: "ecdsa-sha2-nistp256",
+                    plain: "ecdsa-sha2-nistp256",
+                    short_name: "ECDSA",
+                    is_cert: false,
+                    kind: KeyTypeKind::EcdsaNistp256,
+                },
+            "ecdsa-sha2-nistp384" =>
+                KeyType {
+                    name: "ecdsa-sha2-nistp384",
+                    plain: "ecdsa-sha2-nistp384",
+                    short_name: "ECDSA",
+                    is_cert: false,
+                    kind: KeyTypeKind::EcdsaNistp384,
+                },
+            "ecdsa-sha2-nistp521" =>
+                KeyType {
+                    name: "ecdsa-sha2-nistp521",
+                    plain: "ecdsa-sha2-nistp521",
+                    short_name: "ECDSA",
+                    is_cert: false,
+                    kind: KeyTypeKind::EcdsaNistp521,
                 },
             "ecdsa-sha2-nistp256-cert-v01@openssh.com" =>
                 KeyType {
                     name: "ecdsa-sha2-nistp256-cert-v01@openssh.com",
+                    plain: "ecdsa-sha2-nistp256",
                     short_name: "ECDSA-CERT",
                     is_cert: true,
-                    kind: KeyTypeKind::KeyEcdsaCert,
+                    kind: KeyTypeKind::EcdsaNistp256Cert,
                 },
             "ecdsa-sha2-nistp384-cert-v01@openssh.com" =>
                 KeyType {
                     name: "ecdsa-sha2-nistp384-cert-v01@openssh.com",
+                    plain: "ecdsa-sha2-nistp384",
                     short_name: "ECDSA-CERT",
                     is_cert: true,
-                    kind: KeyTypeKind::KeyEcdsaCert,
+                    kind: KeyTypeKind::EcdsaNistp384Cert,
                 },
             "ecdsa-sha2-nistp521-cert-v01@openssh.com" =>
                 KeyType {
                     name: "ecdsa-sha2-nistp521-cert-v01@openssh.com",
+                    plain: "ecdsa-sha2-nistp521",
                     short_name: "ECDSA-CERT",
                     is_cert: true,
-                    kind: KeyTypeKind::KeyEcdsaCert,
+                    kind: KeyTypeKind::EcdsaNistp521Cert,
+                },
+            "ssh-ed25519" =>
+                KeyType {
+                    name: "ssh-ed25519",
+                    plain: "ssh-ed25519",
+                    short_name: "ED25519",
+                    is_cert: false,
+                    kind: KeyTypeKind::Ed25519,
+                },
+            "ssh-ed25519-cert-v01@openssh.com" =>
+                KeyType {
+                    name: "ssh-ed25519-cert-v01@openssh.com",
+                    plain: "ssh-ed25519",
+                    short_name: "ED25519-CERT",
+                    is_cert: true,
+                    kind: KeyTypeKind::Ed25519Cert,
                 },
             _ => return Err(Error::with_kind(ErrorKind::UnknownKeyType(name.to_string()))),
         };
