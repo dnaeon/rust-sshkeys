017df3128381787bf43ea5843c74092957678545
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-09-25 14:08:40 +0300

    lib: re-export names

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,17 @@ extern crate sha2;
 
 // TODO: Should all be public?
 pub mod cert;
+pub mod curve;
 pub mod reader;
 pub mod writer;
 pub mod error;
 pub mod pubkey;
 pub mod keytype;
 
+pub use self::curve::{Curve, CurveKind};
 pub use self::cert::{CertType, Certificate};
 pub use self::reader::Reader;
 pub use self::writer::Writer;
 pub use self::error::{Error, ErrorKind, Result};
-pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey};
+pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey, DsaPublicKey, EcdsaPublicKey};
 pub use self::keytype::{KeyType, KeyTypeKind};
