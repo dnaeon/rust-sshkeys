eb537887bd82a1abbc062e97ed48595d6c3fe9c1
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-10-05 17:10:16 +0300

    lib: style fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,18 @@ extern crate sha2;
 // TODO: Should all be public?
 pub mod cert;
 pub mod curve;
+pub mod error;
+pub mod keytype;
+pub mod pubkey;
 pub mod reader;
 pub mod writer;
-pub mod error;
-pub mod pubkey;
-pub mod keytype;
 
 pub use self::curve::{Curve, CurveKind};
 pub use self::cert::{CertType, Certificate};
+pub use self::error::{Error, ErrorKind, Result};
+pub use self::keytype::{KeyType, KeyTypeKind};
+pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey, DsaPublicKey, EcdsaPublicKey, Ed25519PublicKey, Fingerprint, FingerprintKind};
 pub use self::reader::Reader;
 pub use self::writer::Writer;
-pub use self::error::{Error, ErrorKind, Result};
-pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey, DsaPublicKey, EcdsaPublicKey, Ed25519PublicKey, Fingerprint, FingerprintKind};
-pub use self::keytype::{KeyType, KeyTypeKind};
+
+
