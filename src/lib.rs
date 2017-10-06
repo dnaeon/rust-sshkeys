b3c825f8261e5be7990f0db8f030bdf5a39c09c6
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-10-06 17:08:41 +0300

    lib: initial commit of module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,42 @@
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 
+//! The `sshkeys` crate provides types and methods for parsing
+//! OpenSSH public keys and certificates.
+//!
+//! The following public key types are supported.
+//!
+//! - RSA
+//! - DSA
+//! - ECDSA
+//! - ED25519
+//!
+//! The following OpenSSH certificate types are supported as well.
+//!
+//! - ssh-rsa-cert-v01@openssh.com
+//! - ssh-dss-cert-v01@openssh.com
+//! - ecdsa-sha2-nistp256-cert-v01@openssh.com
+//! - ecdsa-sha2-nistp384-cert-v01@openssh.com
+//! - ecdsa-sha2-nistp512-cert-v01@openssh.com
+//! - ssh-ed25519-cert-v01@openssh.com
+//!
+//! # Examples
+//!
+//! ```rust
+//! # use sshkeys;
+//! # fn run() -> sshkeys::Result<()> {
+//! let key = sshkeys::PublicKey::from_path("/path/to/my-public-key.pub")?;
+//! println!("Key bits: {}", key.bits());
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! TODO: Add more examples
+
 extern crate base64;
 extern crate byteorder;
 extern crate sha2;
 
-// TODO: Should all be public?
 pub mod cert;
 pub mod curve;
 pub mod error;
