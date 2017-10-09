c8db5f34d187a801d1f53e022a3c4738c3688ec4
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-10-09 16:00:30 +0300

    lib: make modules private and re-export only what's needed

--- src/lib.rs
+++ src/lib.rs
@@ -38,20 +38,16 @@ extern crate base64;
 extern crate byteorder;
 extern crate sha2;
 
-pub mod cert;
-pub mod curve;
-pub mod error;
-pub mod keytype;
-pub mod pubkey;
-pub mod reader;
-pub mod writer;
-
-pub use self::curve::{Curve, CurveKind};
 pub use self::cert::{CertType, Certificate};
-pub use self::error::{Error, ErrorKind, Result};
+pub use self::error::{Error, Result};
 pub use self::keytype::{KeyType, KeyTypeKind};
-pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey, DsaPublicKey, EcdsaPublicKey, Ed25519PublicKey, Fingerprint, FingerprintKind};
+pub use self::pubkey::{PublicKey, PublicKeyKind, RsaPublicKey, DsaPublicKey, EcdsaPublicKey, Ed25519PublicKey, Fingerprint, FingerprintKind, Curve, CurveKind};
 pub use self::reader::Reader;
 pub use self::writer::Writer;
 
-
+mod cert;
+mod error;
+mod keytype;
+mod pubkey;
+mod reader;
+mod writer;
