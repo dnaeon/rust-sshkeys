b802b064a9381c85c63f8a7e5b3ba3b7a1173860
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-10-16 14:35:54 +0300

    reader: add doc tests

--- src/reader.rs
+++ src/reader.rs
@@ -16,16 +16,16 @@ impl<'a> Reader<'a> {
     /// # Example
     /// ```rust
     /// # use sshkeys;
-    /// # fn example() -> sshkeys::Result<()> {
     /// let data = vec![0, 0, 0, 42];
     /// let mut reader = sshkeys::Reader::new(&data);
-    /// let num = reader.read_u32()?;
+    /// let num = reader.read_u32().unwrap();
     /// assert_eq!(num, 42);
-    /// # Ok(())
-    /// # }
     /// ```
     pub fn new<T: ?Sized + AsRef<[u8]>>(inner: &T) -> Reader {
-        Reader { inner: inner.as_ref(), offset: 0, }
+        Reader {
+            inner: inner.as_ref(),
+            offset: 0,
+        }
     }
 
     /// Sets the `Reader` current offset to a given position.
@@ -33,16 +33,13 @@ impl<'a> Reader<'a> {
     /// # Example
     /// ```rust
     /// # use sshkeys;
-    /// # fn example() -> sshkeys::Result<()> {
     /// let data = vec![0, 0, 0, 42];
     /// let mut reader = sshkeys::Reader::new(&data);
-    /// let num = reader.read_u32()?;
+    /// let num = reader.read_u32().unwrap();
     /// assert_eq!(num, 42);
     /// reader.set_offset(0);
-    /// let num_42_again = reader.read_u32()?;
+    /// let num_42_again = reader.read_u32().unwrap();
     /// assert_eq!(num_42_again, 42);
-    /// # Ok(())
-    /// # }
     /// ```
     pub fn set_offset(&mut self, offset: usize) -> Result<()> {
         self.offset = offset;
@@ -50,10 +47,19 @@ impl<'a> Reader<'a> {
         Ok(())
     }
 
-    /// Reads a `string` value from the wrapped byte sequence and
+    /// Reads a byte buffer from the wrapped byte sequence and
     /// returns it as a `Vec<u8>`.
-    /// A `string` is represented by it's length as `u32` value
-    /// followed by the bytes to read.
+    /// The buffer is represented by it's length as `u32` value
+    /// followed by the actual bytes to read.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use sshkeys;
+    /// let data = vec![0, 0, 0, 13, 97, 32, 116, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103];
+    /// let mut reader = sshkeys::Reader::new(&data);
+    /// let bytes = reader.read_bytes().unwrap();
+    /// assert_eq!(bytes, [97, 32, 116, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103]);
+    /// ```
     pub fn read_bytes(&mut self) -> Result<Vec<u8>> {
         if self.offset >= self.inner.len() {
             return Err(Error::with_kind(ErrorKind::UnexpectedEof));
@@ -78,7 +84,17 @@ impl<'a> Reader<'a> {
     }
 
     /// Reads an `mpint` value from the wrapped byte sequence.
+    ///
     /// Drops the leading byte if it's value is zero according to the RFC 4251, section 5.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use sshkeys;
+    /// let data = vec![0, 0, 0, 3, 1, 0, 1];
+    /// let mut reader = sshkeys::Reader::new(&data);
+    /// let mpint = reader.read_mpint().unwrap();
+    /// assert_eq!(mpint, [1, 0, 1]);
+    /// ```
     pub fn read_mpint(&mut self) -> Result<Vec<u8>> {
         let bytes = self.read_bytes()?;
 
@@ -90,8 +106,16 @@ impl<'a> Reader<'a> {
     }
 
     /// Reads a `string` value from the wrapped byte sequence and
-    /// returns it as a `String`.
-    /// The value that we read should be a valid UTF-8.
+    /// returns it as a `String`. The value that we read should be a valid UTF-8.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use sshkeys;
+    /// let data = vec![0, 0, 0, 13, 97, 32, 116, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103];
+    /// let mut reader = sshkeys::Reader::new(&data);
+    /// let result = reader.read_string().unwrap();
+    /// assert_eq!(result, "a test string");
+    /// ```
     pub fn read_string(&mut self) -> Result<String> {
         let bytes = self.read_bytes()?;
         let result = String::from_utf8(bytes)?;
@@ -100,6 +124,15 @@ impl<'a> Reader<'a> {
     }
 
     /// Reads an `u32` value from the wrapped byte sequence and returns it.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use sshkeys;
+    /// let data = vec![0, 0, 0, 42];
+    /// let mut reader = sshkeys::Reader::new(&data);
+    /// let num = reader.read_u32().unwrap();
+    /// assert_eq!(num, 42);
+    /// ```
     pub fn read_u32(&mut self) -> Result<u32> {
         if self.offset >= self.inner.len() {
             return Err(Error::with_kind(ErrorKind::UnexpectedEof));
@@ -117,6 +150,15 @@ impl<'a> Reader<'a> {
     }
 
     /// Reads an `u64` value from the wrapped byte sequence and returns it.
+    ///
+    /// # Example
+    /// ```rust
+    /// # use sshkeys;
+    /// let data = vec![0, 0, 0, 0, 0, 0, 0, 42];
+    /// let mut reader = sshkeys::Reader::new(&data);
+    /// let num = reader.read_u64().unwrap();
+    /// assert_eq!(num, 42);
+    /// ```
     pub fn read_u64(&mut self) -> Result<u64> {
         if self.offset >= self.inner.len() {
             return Err(Error::with_kind(ErrorKind::UnexpectedEof));
